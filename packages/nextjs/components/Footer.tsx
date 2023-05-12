e74fe50f1676c777a9c08c316b0da6ccb219a479
Zak G <zakgriffith@gmail.com>
2023-05-12 15:44:46 -0600

    Commit dao-llc branch

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,30 +1,13 @@
-import { hardhat } from "wagmi/chains";
-import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
-import { HeartIcon } from "@heroicons/react/24/outline";
 import { SwitchTheme } from "~~/components/SwitchTheme";
-import { Faucet } from "~~/components/scaffold-eth";
-import { useGlobalState } from "~~/services/store/store";
-import { getTargetNetwork } from "~~/utils/scaffold-eth";
 
 /**
  * Site footer
  */
 export const Footer = () => {
-  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
-
   return (
     <div className="min-h-0 p-5 mb-11 lg:mb-0">
       <div>
         <div className="fixed flex justify-between items-center w-full z-20 p-4 bottom-0 left-0 pointer-events-none">
-          <div className="flex space-x-2 pointer-events-auto">
-            {nativeCurrencyPrice > 0 && (
-              <div className="btn btn-primary btn-sm font-normal cursor-auto">
-                <CurrencyDollarIcon className="h-4 w-4 mr-0.5" />
-                <span>{nativeCurrencyPrice}</span>
-              </div>
-            )}
-            {getTargetNetwork().id === hardhat.id && <Faucet />}
-          </div>
           <SwitchTheme className="pointer-events-auto" />
         </div>
       </div>
@@ -38,12 +21,12 @@ export const Footer = () => {
                 rel="noreferrer"
                 className="underline underline-offset-2"
               >
-                Fork me
+                Fork Scaffold-ETH2
               </a>
             </div>
             <span>·</span>
             <div>
-              Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
+              Built by the 🏰{" "}
               <a
                 href="https://buidlguidl.com/"
                 target="_blank"
@@ -51,17 +34,6 @@ export const Footer = () => {
                 className="underline underline-offset-2"
               >
                 BuidlGuidl
-              </a>
-            </div>
-            <span>·</span>
-            <div>
-              <a
-                href="https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA"
-                target="_blank"
-                rel="noreferrer"
-                className="underline underline-offset-2"
-              >
-                Support
               </a>
             </div>
           </div>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,58 +1,106 @@
+import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Scaffold-ETH 2 App</title>
-        <meta name="description" content="Created with 🏗 scaffold-eth-2" />
-      </Head>
+  const owners: Array<string> = [
+    "0x2dDC12E691F44f12A4BF5650317321fd996fB2F2",
+    "0x2D143b3Ae28Fa31E7c821D138c58c32A30aA36Ae",
+    "0x60583563D5879C2E59973E5718c7DE2147971807",
+    "0x34aA3F359A9D614239015126635CE7732c18fDF3",
+    "0x60Ca282757BA67f3aDbF21F3ba2eBe4Ab3eb01fc",
+    "0xf7e89E45502890381F9242403eA8661fad89Ca79",
+    "0x5c43B1eD97e52d009611D89b74fA829FE4ac56b1",
+    "0x38F84e92B468a1885e73CedC9A4d5632DE07EABB",
+    "0x2DdA8dc2f67f1eB94b250CaEFAc9De16f70c5A51",
+    "0x775aF9b7c214Fe8792aB5f5da61a8708591d517E",
+  ];
 
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
-          </h1>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/nextjs/pages/index.tsx</code>
+  function Information() {
+    return (
+      <div className="w-2/5">
+        <div className="bg-secondary mt-16 text-center p-8 rounded-lg">
+          <p className="text-xl">
+            <b>Constitution & Governance</b>
           </p>
-          <p className="text-center text-lg">
-            Edit your smart contract <code className="italic bg-base-300 text-base font-bold">YourContract.sol</code> in{" "}
-            <code className="italic bg-base-300 text-base font-bold">packages/hardhat/contracts</code>
+
+          <p>
+            BuidlGuidl DAO (LLC) exists to fund developers building generic tutorials, components, and prototypes
+            improve the 🏗 scaffold-eth and greater Ethereum ecosystem.
+          </p>
+
+          <p>The BuidlGuidl DAO is member managed, utilizing a Safe smart contract at address:</p>
+
+          <Link href="https://etherscan.io/address/0x0eb2AB241210900Aeac2fbA054dD605355fe2490" target="_blank">
+            <button className="btn btn-primary">0x0eb2AB241210900Aeac2fbA054dD605355fe2490</button>
+          </Link>
+
+          <p>
+            Members vote to send funds to 🏰 BuidlGuidl developers via streams and scholarships. Members can also to
+            grant funds to external developers and educators who are enriching the ecosystem. All voting allotment
+            allotment will take place in the Safe.
+          </p>
+
+          <p>
+            Proposals to change the Constitution, Governance, or update the DAO in any way may only be introduced
+            current members. There are no fees associated with being a member, and any members who leave the entitled to
+            entitled to any funding in the smart contract.
+          </p>
+
+          <p>A 4 out of 10 vote is required to add or remove members or pass an active proposal.</p>
+
+          <p>DAO members all have equal voting rights.</p>
+
+          <p>
+            The DAO will do its best to keep developers&apos; ETH streams on{" "}
+            <a href="https://buidlguidl.com">🏰 BuidlGuidl.com</a> full and flowing.
+          </p>
+
+          <p>
+            <b>Keep 🛠 Buidling</b>
           </p>
         </div>
+      </div>
+    );
+  }
 
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contract
-                </Link>{" "}
-                tab.
-              </p>
+  function Members() {
+    return (
+      <div className="w-2/5">
+        <div className="bg-secondary my-16 text-center rounded-lg">
+          <div>
+            <div className="py-12 font-bold text-2xl">
+              <Link href="https://etherscan.io/address/0x0eb2AB241210900Aeac2fbA054dD605355fe2490" target="_blank">
+                <button className="btn btn-primary">dao.buidlguidl.eth</button>
+              </Link>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <SparklesIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Experiment with{" "}
-                <Link href="/example-ui" passHref className="link">
-                  Example UI
-                </Link>{" "}
-                to build your own UI.
-              </p>
+            <div className="font-bold text-xl"> Members: </div>
+            <div className="text-left ml-10 mr-10 p-8">
+              <ul>
+                {owners.map(item => (
+                  <div key={item} className="p-2 m-2 border-2 border-primary rounded-md">
+                    <Address address={item} />
+                  </div>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className="bg-primary flex flex-col items-center">
+      <Head>
+        <title>BuidlGidl DAO LLC</title>
+        <meta name="BuidlGidl DAO LLC" content="BuidlGuidl DAO LLC Constitution & Governance" />
+      </Head>
+      <Information />
+      <Members />
+    </div>
   );
 };
 
